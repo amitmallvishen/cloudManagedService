2437736d1cf7c705fbd708bffec5a26d9b40a193
aricent <amall@mirantis.com>
2016-04-01 17:28:30 -0700

    changed the code during integration'

--- core/compute_resources.py
+++ core/compute_resources.py
@@ -40,33 +40,47 @@ class compute_resource(object):
       config = InitializationManager()
       config.CONF( default_config_files = ['openstackconfiguration.conf'] )
 
+
+      def __init__( self ):
+          self.db = InitializationManager()
+          self.db.CONF( default_config_files = ['openstackconfiguration.conf'] )
+
+
+      def update_config_db( self ,config_dict ):
+
+          for key in config_dict:
+              if None != config_dict[key]:
+                 self.db.CONF.OpenStackCapacity[key] = config_dict[key]
+
+
       def cal_num_of_instances_for_design( self ):
-          capacity = self.config.CONF.OpenStackCapacity
+          capacity = self.db.CONF.OpenStackCapacity
           return (capacity.Current_number_of_instances * (( 100 + capacity.Instance_scaling_over_6_months ) /float(100) )) 
 
       def cal_no_of_compute_node( self ):
 
-          capacity = self.config.CONF.OpenStackCapacity
-          no_compute_node = ( capacity.Average_vCPUs_per_instance * capacity.Rack_units_per_node / \
+          capacity = self.db.CONF.OpenStackCapacity
+          no_compute_node = ( capacity.Average_vCPUs_per_instance * self.cal_num_of_instances_for_design()  / \
                               ( capacity.CPUs_per_compute_node * capacity.Cores_per_CPU * capacity.Hyperthreading_Factor * capacity.CPU_Oversubscription_Factor ) )
-          no_compute_node1 = capacity.Average_vCPUs_per_instance * self.cal_num_of_instances_for_design() / capacity.Memory_per_compute_node
+
+          no_compute_node1 = capacity.Average_memory_per_instance * self.cal_num_of_instances_for_design() / float(capacity.Memory_per_compute_node)
 
           return round(max(no_compute_node , no_compute_node1 ) )
 
       def cal_total_vcpu( self ):
-          capacity = self.config.CONF.OpenStackCapacity
+          capacity = self.db.CONF.OpenStackCapacity
 
           if True == capacity.Hyperthreading:
               total_vcpu = self.cal_no_of_compute_node() * capacity.CPUs_per_compute_node * capacity.Cores_per_CPU * capacity.Hyperthreading_Factor * capacity.CPU_Oversubscription_Factor
           else:
-              total_vcpu = self. cal_no_of_compute_node() * capacity.CPUs_per_compute_node * capacity.Cores_per_CPU * capacity.Hyperthreading_Factor * capacity.CPU_Oversubscription_Factor
+              total_vcpu = self. cal_no_of_compute_node() * capacity.CPUs_per_compute_node * capacity.Cores_per_CPU * capacity.CPU_Oversubscription_Factor
 
           return total_vcpu
 
 
       def cal_total_memory( self ):
            
-          capacity = self.config.CONF.OpenStackCapacity
+          capacity = self.db.CONF.OpenStackCapacity
           total_mem = self.cal_no_of_compute_node() * capacity.Memory_per_compute_node
 
           return total_mem
@@ -74,13 +88,22 @@ class compute_resource(object):
     
       def cal_instances_based_on_vcpu( self ):
  
-         return	self.cal_total_vcpu() / self.config.CONF.OpenStackCapacity.Hyperthreading_Factor
+         return	self.cal_total_vcpu() / self.db.CONF.OpenStackCapacity.Average_vCPUs_per_instance
 
  
       def cal_instances_based_on_memory ( self ):
-         return self.cal_total_memory() / self.config.CONF.OpenStackCapacity.Average_memory_per_instance
+         return self.cal_total_memory() / self.db.CONF.OpenStackCapacity.Average_memory_per_instance
 
 
+      def calc_os_capacity( self , config_dict = None):
+          if config_dict != None:
+             self.update_config_db( config_dict )
+
+          vcpu = self.cal_instances_based_on_vcpu()
+          memory = self.cal_instances_based_on_memory()
+
+          return vcpu,memory
+ 
 #if __name__ == "__main__":
 
 #    res = compute_resource()
